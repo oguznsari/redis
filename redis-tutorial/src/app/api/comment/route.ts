f0a8cb8df23fd5048f67b8c60cead5af7c6a08c0
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-09-03 23:21:36 +0300

    JSON failed on redis, try later

--- redis-tutorial/src/app/api/comment/route.ts
+++ redis-tutorial/src/app/api/comment/route.ts
@@ -15,7 +15,7 @@ export const POST = async (req: NextRequest) => {
             url: `redis://${REDIS_USERNAME}:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}`
         });
 
-        redisClient.connect().then(() => {
+        await redisClient.connect().then(() => {
             console.info(
                 `NextJS Redis client connected..`
             );
@@ -31,18 +31,29 @@ export const POST = async (req: NextRequest) => {
         
         const comment = {
             text,
+            tags,
+            upvotes: 0,
             timestamp: new Date().toString(),
             author: req.cookies.get('userId')?.value,
         }
-        
+
+        // await Promise.all([
+        //     redisClient.rPush('comments', commentId),   // add comment to list
+        //     redisClient.sAdd(`tags:${commentId}`, tags), // add tags to comment
+        //     // sets -> holds unique values - no duplicates - always unique
+        //     Object.entries(comment).forEach(async ([field, value]) => {
+        //         redisClient.hSet(`comment_details:${commentId}`, field, value);
+        //     }), // retrieve & store the comment details
+        // ])
+        console.log({comment})
+
+        // await redisClient.json.numIncrBy('comment:djKxQ4LIgRCSMBRIC7E8g', '$.upvotes')
         await Promise.all([
-            redisClient.rPush('comments', commentId),   // add comment to list
-            redisClient.sAdd(`tags:${commentId}`, tags), // add tags to comment
-            // sets -> holds unique values - no duplicates - always unique
-            Object.entries(comment).forEach(async ([field, value]) => {
-                redisClient.hSet(`comment_details:${commentId}`, field, value);
-            }), // retrieve & store the comment details
-        ])
+            redisClient.rPush('comments', commentId),
+            // redisClient.json.SET(`comment:${commentId}`, '$', JSON.stringify(comment))
+            // wasn't able to work above
+            redisClient.set(`comment:${commentId}`, JSON.stringify(comment))
+        ]);
 
         return new Response('OK');
     } catch (error) {
